ecbbfe81be33eef09c7237bbcbe89de628be7b03
Tong Mu <tmu@cofo.edu>
2016-10-17 14:11:26 -0500

    first commit for the landing page for the app

--- Pods/Target Support Files/Pods-icebreaker/Pods-icebreaker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_icebreaker : NSObject
-@end
-@implementation PodsDummy_Pods_icebreaker
-@end
--- Pods/Target Support Files/Pods-icebreaker/Pods-icebreaker-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_icebreakerVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_icebreakerVersionString[];
-
